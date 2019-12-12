b3663a89cfde9607b6d438be89bb8bb7402970c2
Joshua <github@itzcrutchie.com>
2019-12-12 15:01:30 -0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -7,7 +7,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Money Washing'
 
-version '0.0.2'
+version '0.1.0'
 
 server_scripts {
 	'@es_extended/locale.lua',
