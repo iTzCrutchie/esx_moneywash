0b82e85bce56adb5741f9388dc38e95d0294453b
Joshua <github@itzcrutchie.com>
2019-12-12 14:35:14 -0600

    Update 0.1.0 from 0.0.2
    - Added authorized jobs to the loactions.
    - Updated config to a more clean look with instructions on adding new locations and jobs
    - Added version.json and version.lua to check for newer versions on server start

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 -------------------------------------------
 ------- [ Money Wash by Crutchie ] --------
------------- [ Version 0.0.2 ] ------------
+------------ [ Version 0.1.0 ] ------------
 -------------------------------------------
 
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
@@ -14,6 +14,7 @@ server_scripts {
 	'locales/en.lua',
 	'config.lua',
 	'server/main.lua',
+	'version.lua'
 }
 
 client_scripts {
--- config.lua
+++ config.lua
@@ -15,13 +15,50 @@ local hour = 60 * minute
 
 Config.timer = 5 * second -- Time it takes to wash money. The * amount will determine if its hours, second, or minutes.
 
-Config.Zones = {
+--[[ 
+	Below are the zones for laundering. You can set multiple zones just follow the format below. 
+	Failure to do so will result in the script breaking.
+	Set the job to 'any' if you want anybody to use the location. Otherwise set the required job you want to use for the location
+	Any job not allowed to use the location WILL NOT see a marker or get a popup to use it.
 
-	laundryMat = {
+	
+	EXAMPLE LOCATION!!! JUST COPY THIS AND PASTE TO ADD MORE LOCATIONS!
+		
+	{
 		Pos = {
-			{x = 1122.39 , y = -3194.86 , z = -41.60 },
-			--{x = 136.92 , y = -1278.44 , z = 28.35}
+			{x = 1090.84 , y = -2233.43 , z = 31.5}
+		},
+		
+		Jobs = {
+			--'any', -- SET THE 'any' TAG TO ALLOW ALL JOBS INCLUDING POLICE TO USE THE LOCATION
+			--'miner',
+			--'cardealer'
 		}
-	}
+	},
+	
+]]
 
+Config.Zones = {
+	
+	{	
+		Pos = { 
+			{x = 1122.5 , y = -3194.98 , z = -41.60},
+		},
+		
+		Jobs = {
+			--'any', -- set to 'any' to allow the location for any player regardless of job
+			'cardealer'
+		}
+	},
+	
+	--[[{
+		Pos = {
+			{x = 1090.84 , y = -2233.43 , z = 31.5}
+		},
+		
+		Jobs = {
+			--'miner',
+			'cardealer'
+		}
+	},]]
 }
